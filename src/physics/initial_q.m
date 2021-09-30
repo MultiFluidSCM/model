6a6cbab2b55f89cc0b3d19cd42904e22a3d3732f
statisdisc <will.a.m@hotmail.co.uk>
2021-09-30 16:52:34 +0100

    Tidied up notation in initial_q.m

--- src/physics/initial_q.m
+++ src/physics/initial_q.m
@@ -2,15 +2,15 @@ function qv = initial_q( z, initial )
 %INITIAL_Q Set initial water vapour mixing ratio
 %   Provides a water vapour as a function of height z
 
-initial.rv = initial.rv./(1 + initial.rv);
+initial_qv = initial.rv./(1 + initial.rv);
 
 for i=1:length(initial.z)-1
    if (initial.z(i) <= z && z < initial.z(i+1))
-        qv = initial.rv(i)+(initial.rv(i+1)-initial.rv(i)) .* (z-initial.z(i))./(initial.z(i+1)-initial.z(i)) ;
+        qv = initial_qv(i)+(initial_qv(i+1)-initial_qv(i)) .* (z-initial.z(i))./(initial.z(i+1)-initial.z(i)) ;
    end
 end
 if z >= initial.z(end)
-    qv = initial.rv(end);
+    qv = initial_qv(end);
 end
 
 end
